d56684ae926eea0a2c33e02827031cf6ee87281c
Matt Bowler <mbowler03@gmail.com>
2021-12-06 13:46:07 -0500

    first commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+
+const About = () => {
+    return (
+        <Fragment>
+            <h1>Hands on React project</h1>
+            <p>Based on a Udemy course by Brad Traversy</p>
+            <p>Originally made with classes then refactored to use Hooks and the Context Api</p>
+        </Fragment>
+    )
+}
+
+export default About
